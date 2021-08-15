e19faae69223a261a40d0044fee4056e0b4bc622
rosindubh <p.welsby2@ntlworld.com>
2021-08-15 09:41:43 +0100

    comment out console.log enteries

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ let list = document.getElementsByTagName("ul") [0];
 
 submitBtn.addEventListener("click", () => {
     let listItem = document.createElement("li");
-    console.log("listItem", listItem)
-    console.log("list", list);
+    //console.log("listItem", listItem)
+    //console.log("list", list);
     listItem.textContent = input.value;
-    /*console.log(listItem)*/
+    //console.log(listItem)
     list.appendChild(listItem);
     input.value = "";
 
 removeBtn.addEventListener('click', () => {
     let lastItem = document.querySelector('li:last-child');
-    let list = document.getElementsByTagName('ul')[0];
+    //let list = document.getElementsByTagName('ul')[0];
     list.removeChild(lastItem);
+
     })
 })
 
