4b2c7137d6d28590c4209b64dcac54f9cb9c4a38
rosindubh <p.welsby2@ntlworld.com>
2021-08-15 13:00:50 +0100

    found bug causing multiple entries being removed. [removeBtn] was nested inside [submitBtn]

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ const submitBtn = document.getElementById("submitBtn");
 const removeBtn = document.getElementById('remove');
 let list = document.getElementsByTagName("ul") [0];
 
-
+//add item to list
 submitBtn.addEventListener("click", () => {
     let listItem = document.createElement("li");
     listItem.textContent = input.value;
     list.appendChild(listItem);
     input.value = "";
+})
 
+//remove item from list
 removeBtn.addEventListener('click', () => {
     let lastItem = document.querySelector('li:last-child');
     //let list = document.getElementsByTagName('ul')[0];
     list.removeChild(lastItem);
 
     })
-})
-
-
-
 
